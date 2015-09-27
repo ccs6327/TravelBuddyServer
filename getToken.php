c4f417dbce5f2a7de4660b4467d6e0655516bc61
CCS <ccs6327@gmail.com>
2015-09-26 17:35:23 -0700

    make to post request

--- getToken.php
+++ getToken.php
@@ -2,17 +2,17 @@
 	require('/vendor/autoload.php');
 	use OpenTok\OpenTok;
 	use OpenTok\Session;
-	if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-		//TODO move API key, sessionID to client side request headers
-		$opentok = new OpenTok('45355412', 
-			'd14bc579a363bc2dee91f2de51f8ae23898fb664');
-		$token = $opentok->generateToken($_GET["sessionId"]);
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		if (isset($_POST['apiKey'])) {
+			$opentok = new OpenTok($_POST['apiKey'], 
+				'd14bc579a363bc2dee91f2de51f8ae23898fb664');
+			$token = $opentok->generateToken($_POST["sessionId"]);
 
-		$arr = array (
-			'token' => $token,
-		);
-		echo json_encode($arr);
-		//TODO save to database
+			$arr = array (
+				'token' => $token,
+			);
+			echo json_encode($arr);
+		}
 	} else {
 		echo "WRONG METHOD";
 	}
