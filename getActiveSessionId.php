1a228ac3472b5ceda0b2f11c8237ebcc390cbc0d
ccs6327 <ccs6327@gmail.com>
2015-09-27 02:31:56 -0700

    get active sessions from the server

---
+++ getActiveSessionId.php
@@ -0,0 +1,27 @@
+<?php
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "travel_buddy";
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		if ($conn->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		} 
+
+		$sql = "SELECT (sessionId) FROM session";
+		$result = $conn->query($sql);
+
+		if ($result->num_rows > 0) {
+			$row = $result->fetch_array(MYSQLI_NUM);		
+			echo $row[0];
+		} else {
+		    erro_log("Error: " . $sql . "<br>" . $conn->error);
+		}	
+	} else {
+		echo "WRONG METHOD";
+	}
+?>
